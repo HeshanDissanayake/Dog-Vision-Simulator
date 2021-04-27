2183c056d1a6c538bb2ef98bd8195d4e8a9652c2
heshads <tharindudissa18@gmail.com>
2021-04-27 06:08:42 +0530

    Sticher class initiated

--- ipcamera.py
+++ ipcamera.py
@@ -1,10 +1,14 @@
 import threading
-
-class ipcamera(threading.Thread):
+import cv2
+class IpCamera(threading.Thread):
     def __init__(self,ip):
         threading.Thread.__init__(self)
         self.ip = ip
         self.frame = None
-    
-    def run():
-        
+        self.cap = cv2.VideoCapture(ip)
+        self.available = False
+    def run(self):
+
+        while True:
+            _, self.frame = self.cap.read()
+            self.available = True
